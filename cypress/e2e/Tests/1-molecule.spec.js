f8ef018a9b720c3eb04c62c7aaf91f4eff229943
Elena Dyagileva <edyagileva96@gmail.com>
2023-05-17 19:07:23 +0800

    fixed bugs

--- cypress/e2e/Tests/1-molecule.spec.js
+++ cypress/e2e/Tests/1-molecule.spec.js
@@ -3,20 +3,38 @@ describe("home page tests", () => {
     cy.visit("https://www.molecule.to/");
   });
 
-  it("nav tests", () => {
-    cy.get(".nav-container a").eq(0).should("have.attr", "href", "/");
+  it.only("tests nav links", () => {
+    //cy.get(".nav-container a").eq(0).should("have.attr", "href", "/");
 
     const links = [
-      "About Us",
-      "Blog",
-      "Documentation",
-      "Careers",
-      "Contact",
-      "Launch Marketplace",
+      "/about-us",
+      "/blog",
+      "https://docs.molecule.to/",
+      "https://moleculeto.notion.site/moleculeto/Open-roles-Molecule-a04512c367904c4599d046638f55bee4",
+      "https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=info@molecule.to",
+      "https://discover.molecule.to/",
     ];
 
-    links.forEach((link) => {
-      cy.get(".main-menu-container").should("contain", link);
+    links.forEach((link, i) => {
+      cy.get(".mobile-top-links a").eq(i).should("have.attr", "href", link);
+      //.and("be.visible");
     });
+
+    cy.get(".nav-container a")
+      .last()
+      .should("have.css", "background-color", "rgb(1, 33, 58)");
+  });
+
+  it("tests home page", () => {
+    cy.get("h1").should("be.visible").and("contain", "The future of medicine");
+
+    cy.get(".banner-btns-container a")
+      .eq(0)
+      .should("be.visible")
+      .and("have.attr", "href", "https://discover.molecule.to/");
+    cy.get(".banner-btns-container a")
+      .eq(1)
+      .should("be.visible")
+      .and("have.attr", "href", "https://discord.com/invite/uAGW7K4hQU");
   });
 });
